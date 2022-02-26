cb8304dfd7ed12771f1104b01a5b9aaf4f5d13d6
Kevin van Kessel <vankessel@users.noreply.github.com>
2022-02-26 12:14:05 -0800

    Upload dist files

--- webpack.config.js
+++ webpack.config.js
@@ -51,7 +51,7 @@ module.exports = {
   },
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'public/dist'),
     publicPath: '/dist/', // The leading slash is important!
   },
 };
